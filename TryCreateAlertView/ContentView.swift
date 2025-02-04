fc56c1fc52d4344c7edae1609e745d0aeb40f0f1
Weerawut <weerawut@live.com>
2025-02-04 12:20:02 +0700

    Initial Commit

---
+++ TryCreateAlertView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TryCreateAlertView
+//
+//  Created by Weerawut Chaiyasomboon on 4/2/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TryCreateAlertView/TryCreateAlertViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryCreateAlertViewApp.swift
+//  TryCreateAlertView
+//
+//  Created by Weerawut Chaiyasomboon on 4/2/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct TryCreateAlertViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
